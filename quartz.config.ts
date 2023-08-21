5d2bd46ef7861dd3cdc5e62526fb2788b0dbadde
misile00 <02hrvvqhy@mozmail.com>
2023-08-21 22:36:35 +0300

    Quartz sync: Aug 21, 2023, 10:36 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -51,7 +51,7 @@ const config: QuartzConfig = {
       Plugin.SyntaxHighlighting(),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false, wikilinks: true }),
       Plugin.GitHubFlavoredMarkdown(),
-
+      Plugin.CrawlLinks({ markdownLinkResolution: "absolute"}),
       Plugin.Latex({ renderEngine: "katex" }),
       Plugin.Description(),
     ],
